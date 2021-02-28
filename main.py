4c7b291507622b51fcc1d9ae0648d48c78f72ddb
Maj <maj.knez2@gmail.com>
2021-02-28 22:43:32 +0100

    Fixed issue when non of the programs were open it would crash. -Added support PyCharm Project name and File name that ur editing

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
-import psutil
 from pypresence import Presence
-import time
 import os
 from win32gui import GetWindowText, GetForegroundWindow
-
+import time
 from utils.WindowUtil import getActiveProcessName
+import threading
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -15,8 +13,8 @@ hoverText = os.environ.get("hoverText")
 
 RPC = Presence(client_id,pipe=0)
 RPC.connect()
-print(getActiveProcessName())
 
+timestamp = time.time()
 
 
 
@@ -24,16 +22,17 @@ while True:  # The presence will stay on as long as the program is running
 
     window_name = getActiveProcessName()
     if window_name == "chrome.exe":
-        print(window_name)
+
         RPC.update(details=GetWindowText(GetForegroundWindow()), large_image="chrome", large_text=hoverText, small_image="dnd")
     elif window_name == "pycharm64.exe":
-        print(window_name)
-        print("PyCharm is running")
-        RPC.update(details="Working on " + GetWindowText(GetForegroundWindow()), large_image="pycharm", large_text=hoverText, small_image="online")
-    else:
-        continue
-        #RPC.update(details="Relaxing", large_image="relax", large_text=hoverText, small_image="dnd",
-        #          small_text="Do not disturb", buttons=[{"label": "Join server", "url": "https://discord.gg/dxVxWZejYG"}])
+
+        #print("PyCharm is running")
+        project_name = GetWindowText(GetForegroundWindow()).split()
+        #print(project_name[0])
+        RPC.update(details="Working on \n" + str(project_name[0]),state="Editing "+ str(project_name[2]), large_image="pycharm", large_text=hoverText, small_image="online", start=timestamp)
+    elif window_name:
+        RPC.update(details="Relaxing", large_image="relax", large_text=hoverText, small_image="dnd",
+                  small_text="Do not disturb", buttons=[{"label": "My Website", "url": "https://qtqt.cf"}])
 
 
-    time.sleep(2) # Can only update rich presence every 15 seconds
+    time.sleep(2) # Can only update rich presence every 2 seconds
