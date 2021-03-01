f57091f6129302ec3723e47c4a828625e6b61b03
Maj <maj.knez2@gmail.com>
2021-03-01 11:44:08 +0100

    -Added more features -Added Youtube Music with music title -Added Youtube support with video title -Still have to add full stack overflow support (only supports main page for now)

--- main.py
+++ main.py
@@ -17,21 +17,61 @@ timestamp = time.time()
 
 
 
-while True:  # The presence will stay on as long as the program is running
+while True:
+    try:
+        website = GetWindowText(GetForegroundWindow()).split()
+        window_name = getActiveProcessName()
+        stackoverflow = website[0] + " " + website[1]
+        removeChrome = website[-2] + " " + website[-1]
+    except:
+        print("Error handler:")
+        RPC.update(details="Relaxing", large_image="relax", large_text=hoverText, small_image="dnd",
+                   small_text="Do not disturb",
+                   buttons=[{"label": "Join Discord", "url": "https://discord.gg/dxVxWZejYG"}])
+    print(removeChrome)
+    #Chrome support only for now
 
-    window_name = getActiveProcessName()
     if window_name == "chrome.exe":
 
-        RPC.update(details=GetWindowText(GetForegroundWindow()), large_image="chrome", large_text=hoverText, small_image="dnd")
+        if website[-4] == "YouTube":
+
+            print("you are on youtube")
+            RPC.update(details=GetWindowText(GetForegroundWindow()),state="On YouTube", large_image="youtube", large_text=hoverText,
+                       small_image="dnd")
+        elif website[-4] == "Search":
+            print("you are on stack google search")
+            RPC.update(details=GetWindowText(GetForegroundWindow()), state="On stack overflow", large_image="googlesearch",
+                       large_text=hoverText,
+                       small_image="dnd")
+        elif stackoverflow == "Stack Overflow":
+            del website[-3:]
+            final = " "
+
+
+
+            RPC.update(details=final.join(website), state="On stack overflow",
+                       large_image="stackoverflow",
+                       large_text=hoverText,
+                       small_image="dnd")
+        elif removeChrome == "YouTube Music":
+
+            del website[-3:]
+            final = " "
+
+            RPC.update(details=final.join(website), state="YouTube Music", large_image="youtubemusic",
+                       large_text=hoverText,
+                       small_image="dnd")
+
+
     elif window_name == "pycharm64.exe":
 
         #print("PyCharm is running")
         project_name = GetWindowText(GetForegroundWindow()).split()
         #print(project_name[0])
         RPC.update(details="Working on \n" + str(project_name[0]),state="Editing "+ str(project_name[2]), large_image="pycharm", large_text=hoverText, small_image="online", start=timestamp)
-    elif window_name:
+    else:
         RPC.update(details="Relaxing", large_image="relax", large_text=hoverText, small_image="dnd",
                   small_text="Do not disturb", buttons=[{"label": "Join Discord", "url": "https://discord.gg/dxVxWZejYG"}])
 
 
-    time.sleep(2) # Can only update rich presence every 2 seconds
+    time.sleep(5)
